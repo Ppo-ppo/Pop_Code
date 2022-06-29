0492102c68d58fd054e060f0d2710f56efb35642
Ppo-ppo <pauline-marie.louvrier@outlook.fr>
2022-06-29 16:18:46 +0200

    loader+Home page

--- js.js
+++ js.js
@@ -1,10 +1,13 @@
-//  let loader=document.querySelector('.rond');
-//              let logo=document.querySelector('.logo');
-//              setTimeout(remplacertruc, 10000);
-//              function remplacertruc(){
-//                  loader.style.display="none"
-//                  logo.style.display="block"
-//              }
+  let plusla =document.querySelector('#bobox1')
+  let loader=document.querySelector('.box1');
+              let acceuil=document.querySelector('.acceuil');
+              setTimeout(remplacertruc, 7000);
+              function remplacertruc(){
+                  plusla.classList.add('box1')
+                  acceuil.classList.remove('acceuil')
+              }
+            
+              console.log(acceuil)
 let toto ='...'
 function typingText(text, speed) {
     var i = 0;
