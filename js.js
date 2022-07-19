c749c5de46b38557dc6dff8a83ffcbe3ba07621c
Ppo-ppo <pauline-marie.louvrier@outlook.fr>
2022-07-19 15:20:26 +0200

    input ok

--- js.js
+++ js.js
@@ -24,6 +24,7 @@
   var modal5=document.querySelector('.modal5');
   var politique=document.querySelector('#polique');
   var modal6=document.querySelector('.modal6')
+  var coche=document.querySelector('.coche');
   const tabdescription=[
     {name:"Html",description:"Le HyperText Markup Language, généralement abrégé HTML ou, dans sa dernière version, HTML5, est le langage de balisage conçu pour représenter les pages web.Ce langage permet :d’écrire de l’hypertexte, d’où son nom, structurer sémantiquement la page, mettre en forme le contenu, créer des formulaires de saisie,d’inclure des ressources multimédias dont des images, des vidéos, et des programmes informatiques, créer des documents interopérables avec des équipements très variés de manière conforme aux exigences de l’accessibilité du web. Il est souvent utilisé conjointement avec le langage de programmation JavaScript et des feuilles de style en cascade (CSS). HTML est inspiré du Standard Generalized Markup Language (SGML). Il s'agit d'un format ouvert.",src:"logos/html.svg"},
     {name:"Css",description:"Les feuilles de style en cascade[1], généralement appelées CSS de l'anglais Cascading Style Sheets, forment un langage informatique qui décrit la présentation des documents HTML et XML. Les standards définissant CSS sont publiés par le World Wide Web Consortium (W3C). Introduit au milieu des années 1990, CSS devient couramment utilisé dans la conception de sites web et bien pris en charge par les navigateurs web dans les années 2000.",src:"logos/css.svg"},
@@ -124,6 +125,12 @@ if (document.querySelector('.text').value !== ''){
                 document.querySelector('.modalContent4>h2').textContent=tabdescription[i].name;
                 document.querySelector('.modalContent4>p').textContent=tabdescription[i].description;
                 document.querySelector('.modalContent4>.lg').src=tabdescription[i].src;
+                if(coche.checked === true){
+                    setTimeout(ferme, 2000);
+                        function ferme(){
+                        modal4.classList.toggle('none')
+                    }
+                }
                 document.querySelector('.modal4').classList.toggle('none'),
                 score++ 
                 if(score===reponce.length){
